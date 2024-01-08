3d87f4efd01b8be00a55af74b2215ba09073799b
Coulibaly <ibrahima.coulibaly@le-campus-numerique.fr>
2024-01-08 10:48:57 +0100

    Les variables

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,24 @@
 // you can write js here
-console.log('hello from file');
+// Q1
+//let kelvins = 294
+var kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+console.log("Valeur en kelvins : " + kelvins)
+
+//Q2
+//Je converti le kelvins - celcius en soustrant -273 et l'affecter à la variable celsius
+let celsius = kelvins - 273
+console.log("Valeur en celsius : " + celsius)
+//Q3
+let fahrenheits = celsius * (9/5) + 32
+console.log("Valeur en fahrenheits decimal : " + fahrenheits)
+
+//Q4
+let entierFahrenheits = Math.floor(fahrenheits)
+console.log("Valeur en fahrenheits entier : " + entierFahrenheits)
+
+
+
+
+
+
+
