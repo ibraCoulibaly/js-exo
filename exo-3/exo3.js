7bd74522f81797deac5b50b5b04b1c1579fc5157
Coulibaly <ibrahima.coulibaly@le-campus-numerique.fr>
2024-01-08 16:21:22 +0100

    Les fonctions

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,2 +1,50 @@
 // you can write js here
+
+
+jouerPerso = ''
+function getPlayerChoice(playerInput){
+    playerInput = prompt("Your choice ( rock, paper, scissors) ?")
+    if(playerInput == "rock" || playerInput == "paper"  || playerInput == "scissors"){
+        jouerPerso = playerInput.toLowerCase()
+    }else{
+        jouerPerso = 'erreur'
+    }
+    return jouerPerso
+}
+
+
+
+jouerOrdi = ''
+function getComputerChoice(){
+    let ordi = Math.floor(Math.random()*3)
+    if(ordi == 0){
+        jouerOrdi = 'rock'
+    }else if(ordi == 1){
+        jouerOrdi = 'peper'
+    }else{
+        jouerOrdi = 'scissors'
+    }
+    return jouerOrdi
+}
+
+function findWinner(playerChoice, computerChoice){
+    playerChoice = getPlayerChoice()
+    computerChoice = getComputerChoice()
+    console.log("Le choix de l'utilisateur : " + playerChoice)
+    console.log("Le choix de l'ordi : " + computerChoice)
+    if(playerChoice == computerChoice){
+        console.log('Resultat : Tied')
+        
+    }else if(((playerChoice == "rock" && computerChoice == "scissors") || (playerChoice == "paper" && computerChoice === "rock") || (playerChoice === "scissors") && (computerChoice === "paper"))){
+        console.log("Resultat : Won")
+     
+    }else{
+        console.log("Resultat : Lost")
+
+    }
+}
+
+findWinner()
+
+
 console.log('exo-3');
