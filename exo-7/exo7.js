767da296423348bce7df9b60713fee1c50bca8de
Coulibaly <ibrahima.coulibaly@le-campus-numerique.fr>
2024-01-09 17:00:36 +0100

    first commit

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -1,3 +1,34 @@
-console.log("exo-7");
+//Une copie de jsonDatas
+const copie = jsonDatas
 
+//Q1  afficher un à un dans la console
+for(let i = 0; i < jsonDatas.length; i++){
+    console.log(jsonDatas[i].type)
+}
+
+//Q2
+
+const traduireObj = {
+    traduire: function(e, descrip){
+        for(let i = 0; i < jsonDatas.length; i++){
+            if(jsonDatas[i].type ==  e && jsonDatas[i].description == ""){
+                jsonDatas[i].description = descrip
+            }
+        }
+    }  
+}
+
+//
+for(let i = 0; i < jsonDatas.length; i++){
+        Object.assign(jsonDatas[i], traduireObj)
+    
+}
+
+
+
+
+
+console.log("exo-7");
 console.log(jsonDatas);
+
+
