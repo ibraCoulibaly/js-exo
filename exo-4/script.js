57c4f0b2b3dec235a44b9aa17412578363df677c
Coulibaly <ibrahima.coulibaly@le-campus-numerique.fr>
2024-01-09 09:10:50 +0100

    Les tableaux commit 2

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,48 +3,47 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+console.log(secretMessage)
 
+//Dernier elt
+let dernierElement = secretMessage[secretMessage.length - 1]
+console.log("Dernier element : " + dernierElement)
 
-//Q1 on peut utiliser la fonction pop() -> renvois le dernier elt, mais supprime l'elt ds le tab original
-function enleverDernier(tab){
-    // let tab1 = tab
-    // return tab1.pop()
-    return tab[tab.length-1]
-}
+//Ajout à la fin
+let ajoutFin = secretMessage.push('to', 'programm')
+console.log("Ajout à la fin : " + ajoutFin)
 
-console.log(enleverDernier(secretMessage))
-
-//Q2
-function ajoutFin(mot, tab){
-    tab.push(mot)
-}
-ajoutFin('to', secretMessage)
-ajoutFin('programm', secretMessage)
-
-//Q3 Changement du mot 'easily' par 'right'
+//Remplacer 'easily' par 'right'
 console.log(secretMessage)
 for(let i = 0; i < secretMessage.length; i++){
     if(secretMessage[i] == 'easily'){
-        secretMessage[i] = 'right'
+        secretMessage.splice(i, 1, 'right')
     }
 }
-
 console.log(secretMessage)
 
+//Suppression du premier element
+let supFirstElement = secretMessage.shift()
+console.log(secretMessage)
 
-//Q4
-function supprimerPremier(tab){
-    return tab.shift()
+//Ajout au début
+let ajoutDebut = secretMessage.unshift('Programming')
+console.log(secretMessage)
+
+//remplacer la série de cinq mots 'get', 'right', 'the', 'first', 'time' pae 'know'
+for(let i = 0; i < secretMessage.length; i++){
+    if(secretMessage[i] == 'get' || secretMessage[i] == 'right' || secretMessage[i] == 'the' || secretMessage[i] == 'first' || secretMessage[i] == 'time'){
+        secretMessage.splice(i, 5, 'know')
+    }
 }
-
-console.log(supprimerPremier(secretMessage))
 console.log(secretMessage)
 
-//Q5
-function ajoutDebut(mot, tab){
-    return tab.unshift(mot)
-}
+//join() the message
+let msgSecret = secretMessage.join(' ')
+console.log(msgSecret)
 
-ajoutDebut('Programming', secretMessage)
 
-console.log(secretMessage)
+
+
+
+console.log('exo-5');
