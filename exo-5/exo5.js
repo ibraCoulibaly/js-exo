a42cddfc45c62da4dbe782d2cb286a0ad89d2a32
Coulibaly <ibrahima.coulibaly@le-campus-numerique.fr>
2024-01-09 10:42:57 +0100

    Les boucles

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,31 @@
 // you can write js here
+// you can write js here
+let input = 'gdjef lbrjbvqfjb ivnrknvqk qjbvjrbjjnv edc'
+let vowels = ['a', 'e', 'i', 'u', 'o', 'y']
+let resultArray = []
+
+
+
+
+// With indexOf
+// for(let j = 0; j < input.length; j++){
+//     if(vowels.indexOf(input[j]) > 0){
+//         resultArray.push(input[j])
+//     }
+// }
+    
+
+
+// Avec parcors imbriquer
+for(let i = 0; i < input.length; i++){
+    for(let j = 0; j < vowels.length; j++){
+        if(input[i] == vowels[j]){
+            resultArray.push(input[i])
+        }
+    }
+}
+
+console.log(resultArray);
+console.log(resultArray.join(' ').toUpperCase());
 
 console.log('exo-5');
