e270eff6a9fdd4216136b49ce0eca05efff81a33
Coulibaly <ibrahima.coulibaly@le-campus-numerique.fr>
2024-01-10 16:52:40 +0100

    modif sur exo 4

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,44 +3,36 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
-console.log(secretMessage)
+//console.log(secretMessage)
 
 //Dernier elt
-let dernierElement = secretMessage[secretMessage.length - 1]
-console.log("Dernier element : " + dernierElement)
+secretMessage.pop()
+//console.log(secretMessage)
 
 //Ajout à la fin
 let ajoutFin = secretMessage.push('to', 'programm')
-console.log("Ajout à la fin : " + ajoutFin)
+//console.log(secretMessage)
 
 //Remplacer 'easily' par 'right'
-console.log(secretMessage)
-for(let i = 0; i < secretMessage.length; i++){
-    if(secretMessage[i] == 'easily'){
-        secretMessage.splice(i, 1, 'right')
-    }
-}
-console.log(secretMessage)
+secretMessage.splice(secretMessage.indexOf('easily'), 1, 'right')
+//console.log(secretMessage)
 
 //Suppression du premier element
 let supFirstElement = secretMessage.shift()
-console.log(secretMessage)
+//console.log(secretMessage)
 
 //Ajout au début
 let ajoutDebut = secretMessage.unshift('Programming')
-console.log(secretMessage)
+//console.log(secretMessage)
 
 //remplacer la série de cinq mots 'get', 'right', 'the', 'first', 'time' pae 'know'
-for(let i = 0; i < secretMessage.length; i++){
-    if(secretMessage[i] == 'get' || secretMessage[i] == 'right' || secretMessage[i] == 'the' || secretMessage[i] == 'first' || secretMessage[i] == 'time'){
-        secretMessage.splice(i, 5, 'know')
-    }
-}
 console.log(secretMessage)
+console.log(secretMessage.splice(5, 5, 'know'))
+//console.log(secretMessage)
 
 //join() the message
 let msgSecret = secretMessage.join(' ')
-console.log(msgSecret)
+//console.log(msgSecret)
 
 
 
